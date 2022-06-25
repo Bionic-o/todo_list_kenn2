0c363cba16d1ccaa6d1315d1f13179df46854857
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-06-25 17:52:58 +0200

    Tuesday Component in progress

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,7 +1,19 @@
+// import Monday from './Content/Task-days/Monday';
+import Monday from './Task-days/Monday';
+import Tuesday from './Task-days/Tuesday';
+import Wednesday from './Task-days/Wednesday';
+import Thursday from './Task-days/Thursday';
+import Friday from './Task-days/Friday';
 function Content () {
     return (
-        "This is Content"
+        <div>
+            <Monday />
+            <Tuesday />
+            <Wednesday />
+            <Thursday />
+            <Friday />
+        </div>
     )
 }
 
-export default Content
+export default Content;
