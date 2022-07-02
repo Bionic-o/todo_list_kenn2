a77f8b917064b988253d49132b36fb9f0bb76cdf
Nico <weiskopf.bionico@gmail.com>
2022-07-02 13:57:14 +0200

    update todo_list, corrupted merge
    wrongly merged the old master into the updated branch....so now i try to merge the updated todo_list into the master

--- src/Content/Task-days/AllTasks.js
+++ src/Content/Task-days/AllTasks.js
@@ -1,93 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Content.css';
 
+const mockTask = [{
+    id: "1",
+    name: "doGrocery",
+    isDone: false
+}, {
+    id: "2",
+    name: "doDishes",
+    isDone: true
+}]
+
 function AllTasks(){
-    const [todos, setTodos] = React.useState([])
-    const [todo, setTodo] = React.useState('')
-    const [todoEditing, setTodoEditing] = React.useState(null)
-    const [editingText, setEditingText] = React.useState("")
+    const [getData, setGetData] = useState("")
+    const [arrayData, setArrayData] = useState(mockTask)
+    //const [doneColor, setDoneColor] = useState()
 
-function handleSubmit(e){
-    e.preventDefault()
+    const inputData = (val) => {
+        setGetData(val.target.value)
+        //console.log(val)
+    }
+    const addTask = () => {
+        setArrayData((prev) => [{id: "free", name: getData, isDone: false}, ...prev])
+        setGetData("")
+    }
+    const deleteTask = (index) => {
+        setArrayData((prev) => [getData, ...prev])
+        // const newList = list.filter((i, itemIndex) => index !== itemIndex) 
 
-
-    const newTodo = {
-        id: new Date().getTime(),
-        text: todo,
-        complete: false
     }
-    setTodos([...todos].concat(newTodo))
-    setTodo("")
-}
-function addItem(e){
-    const todoTextInput = document.getElementById('todo-inputText');
-    const todoText = todoTextInput.value
-    if(!todoText){
-        alert("Please add a task!")
-        return
+    const doneTask = () => {
+        //setDoneColor()
+        /* const todoItem = document.getElementById('todoItems');
+        todoItem.style.backgroundColor = '#39ed24'
+        console.log('Am done!') */
     }
-    todo.id.addEventListener('keydown', function(event){
-        if(event.key === "Enter"){
-            addItem(todo.id)
-        }
-        todoTextInput.value = ''
-    });
-}
-    function deleteTodo(id){
-        const updatedTodos = [...todos].filter((todo) => todo.id !== id)
-
-        setTodos(updatedTodos)
-    }
-    function editTodo(id){
-        const saveBtn = document.getElementById('save-btn');
-        saveBtn.style.display = 'none'
-        const updatedTodos = [...todos].map((todo) =>{
-            if(todo.id === id) {
-                todo.text = editingText
-            }
-            return todo
-        })
-        setTodos(updatedTodos)
-        setTodoEditing(null)
-        setEditingText("")
-    }
-    function TaskDone(e){
-        const todoItems = document.getElementById('todoItems');
-        todoItems.style.backgroundColor = '#39ed24'
-        console.log('Am done!')
-    }
-    
-
+ 
     return(
         <div className="allTasks">
-            <form onSubmit={handleSubmit}>
+            <div>
                 <input type="text" 
-                onChange={(e) => setTodo(e.target.value)} 
-                value={todo} 
+                onChange={inputData} 
+                value={getData} 
                 id="todo-inputText"
-                autoComplete='off'
                 />
-                <button type="submit" id="add-btn" onClick={addItem}>Add Task</button>
-            </form>
-            {todos.map((todo) => 
-            <div key={todo.id} id="todoItems">
-                <div id="editSave">
-                    {todoEditing === todo.id ? 
-                    (<input type="text" 
-                    onChange={(e) => setEditingText(e.target.value)} 
-                    value={editingText} 
-                    id="edit-input"
-                    />) : (<p>{todo.text}</p>)}
-                    
-                    <button onClick={() => editTodo(todo.id)} id="save-btn">Save</button>
-                </div>
-                <div>
-                    <button onClick={TaskDone} id="taskDone">Done</button>
-                    <button onClick={() => setTodoEditing(todo.id)} id="edit-btn">Edit</button>
-                    <button onClick={() => deleteTodo(todo.id)} id="delete-btn">Delete</button>
-                </div>
-            </div>)}
-        </div>
+                <button id="add-btn" onClick={addTask}>Add Task</button>
+            </div>
+                 {arrayData.map((singleTask, index) =>
+                    <div id="todoItems" key={index}>
+                        <div>
+                            <p>{singleTask.name}</p>
+                        </div>
+                        <div>
+                            <button onClick={doneTask}>Done</button>
+                            <button>Edit</button>
+                            <button onClick={() => deleteTask(singleTask.index)}>Delete</button>
+                        </div>
+                    </div> 
+                    )}
+            </div>
     )
 }
 
