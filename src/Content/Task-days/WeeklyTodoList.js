789704be0dba55a4ed0f13faf9b2ce9abbc72810
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-07-08 10:22:48 +0200

    Calendar added

--- src/Content/Task-days/WeeklyTodoList.js
+++ src/Content/Task-days/WeeklyTodoList.js
@@ -8,7 +8,8 @@ function WeeklyTodoList({day}){
         <div className="AllTasks">
             <div className="days">
                 <div>{`${day} tasks`}  </div>
-                <div className='date'>Date/Time</div>
+                <input type="date" id="addDate"/>
+                {/* <div className='date'>Date/Time</div> */}
             </div>
             <AllTasks />
         </div>
